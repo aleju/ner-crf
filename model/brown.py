15295565b4ffbc8735a2e4ec34c8613228143cc8
aleju <kontakt@ajung.name>
2015-08-24 22:09:35 +0200

    Add brown clustering handler

--- model/brown.py
+++ model/brown.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-class W2VClusterDict():
+class BrownClusterDict():
     def __init__(self, filepath):
         self.word_to_cluster = dict()
+        self.word_to_bitchain = dict()
         self.fill_from_file(filepath)
     
     def clear(self):
@@ -11,14 +12,30 @@ class W2VClusterDict():
     
     def fill_from_file(self, filepath):
         with open(filepath, "r") as f:
+            last_count = -1
+            cluster_idx = 1
+            
             for line in f:
                 columns = line.decode("utf-8").strip().split("\t")
-                if len(columns) == 2:
-                    word, cluster_idx = columns
+                if len(columns) == 3:
+                    bitchain, word, count = columns
+                    count = int(count)
+                    
+                    if count < last_count:
+                        cluster_idx += 1
+                    last_count = count
+                    
                     self.word_to_cluster[word] = cluster_idx
-    
+                    self.word_to_bitchain[word] = bitchain
+
     def get_cluster_of(self, word, default=-1):
         if word in self.word_to_cluster:
             return self.word_to_cluster[word]
+        else:
+            return default
+
+    def get_bitchain_of(self, word, default=-1):
+        if word in self.word_to_bitchain:
+            return self.word_to_bitchain[word]
         else:
             return default
