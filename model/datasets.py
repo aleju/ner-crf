11729782e9172f6ad1db9151d592d59601b58cfe
aleju <kontakt@ajung.name>
2015-09-06 18:04:21 +0200

    Optimize styling according to pylint

--- model/datasets.py
+++ model/datasets.py
@@ -6,26 +6,26 @@ import re
 from collections import Counter
 
 # All capitalized constants come from this file
-from config import *
+import config as cfg
 
-def split_to_chunks(of, chunk_size):
+def split_to_chunks(of_list, chunk_size):
     """Splits a list to smaller chunks.
     Args:
-        of: The list/iterable to split.
+        of_list: The list/iterable to split.
         chunk_size: The maximum size of each smaller part/chunk.
     Returns:
         Generator of lists (i.e. list of lists).
     """
-    assert of is not None
+    assert of_list is not None
 
-    for i in range(0, len(of), chunk_size):
-        yield of[i:i + chunk_size]
+    for i in range(0, len(of_list), chunk_size):
+        yield of_list[i:i + chunk_size]
 
 def load_articles(filepath, start_at=0):
     """Loads all articles (documents) from a corpus.
-    
+
     The corpus is expected to be a UTF-8 encoded textfile with one article/document per line.
-    
+
     Args:
         filepath: The filepath to the corpus file.
         start_at: The index of the line to start at. (Default is 0.)
@@ -33,19 +33,20 @@ def load_articles(filepath, start_at=0):
         Generator of Article objects, i.e. list of Article.
     """
     skipped = 0
-    with open(filepath, "r") as f:
-        for article in f:
+    with open(filepath, "r") as handle:
+        for article in handle:
             article = article.decode("utf-8").strip()
-            
+
             if len(article) > 0:
                 if skipped < start_at:
                     skipped += 1
                 else:
                     yield Article(article)
 
-def load_windows(articles, window_size, features=None, every_nth_window=1, only_labeled_windows=False):
+def load_windows(articles, window_size, features=None, every_nth_window=1,
+                 only_labeled_windows=False):
     """Loads smaller windows with a maximum size per window from a generator of articles.
-    
+
     Args:
         articles: Generator of articles, as provided by load_articles().
         window_size: Maximum length of each window (in tokens/words).
@@ -63,7 +64,7 @@ def load_windows(articles, window_size, features=None, every_nth_window=1, only_
     for article in articles:
         # count how many labels there are in the article
         count = article.count_labels()
-        
+
         if count / len(article.tokens) >= 0.10:
             # ignore articles with too many labels, because they are too unrealistic
             # (e.g. in Wikipedia lists of people or disambiguation sites)
@@ -88,7 +89,7 @@ def load_windows(articles, window_size, features=None, every_nth_window=1, only_
 
 def generate_examples(windows, nb_append=None, nb_skip=0, verbose=True):
     """Generates example pairs of feature lists (one per token) and labels.
-    
+
     Args:
         windows: The windows to generate features and labels from, see load_windows().
         nb_append: How many windows to append max or None if unlimited. (Default is None.)
@@ -113,11 +114,12 @@ def generate_examples(windows, nb_append=None, nb_skip=0, verbose=True):
             # chain of features (list of lists of strings)
             feature_values_lists = []
             for word_idx in range(len(window.tokens)):
-                fvl = window.get_feature_values_list(word_idx, SKIPCHAIN_LEFT, SKIPCHAIN_RIGHT)
+                fvl = window.get_feature_values_list(word_idx,
+                                                     cfg.SKIPCHAIN_LEFT, cfg.SKIPCHAIN_RIGHT)
                 feature_values_lists.append(fvl)
             # yield (features, labels) pair
             yield (feature_values_lists, labels)
-            
+
             # print message every nth window
             # and stop if nb_append is reached
             added += 1
@@ -134,7 +136,7 @@ def generate_examples(windows, nb_append=None, nb_skip=0, verbose=True):
 #def cleanup_unicode(in_str):
 #    """Converts unicode strings to ascii.
 #    The function uses mostly unidecode() and contains some additional mappings for german umlauts.
-#    
+#
 #    Args:
 #        in_str: String in UTF-8.
 #    Returns:
@@ -163,21 +165,21 @@ class Article(object):
         text = re.sub(r"[\t ]+", " ", text)
         tokens_str = [token_str.strip() for token_str in text.strip().split(" ")]
         self.tokens = [Token(token_str) for token_str in tokens_str if len(token_str) > 0]
-    
+
     def get_content_as_string(self):
         """Returns the article's content as string.
         This is not neccessarily identical to the original text content, because multi-whitespaces
         are replaced by single whitespaces.
-        
+
         Returns:
             string (article/document content).
         """
         return " ".join([token.word for token in self.tokens])
-    
+
     def get_label_counts(self, add_no_ne_label=False):
         """Returns the count of each label in the article/document.
         Count means here: the number of words that have the label.
-        
+
         Args:
             add_no_ne_label: Whether to count how often unlabeled words appear. (Default is False.)
         Returns:
@@ -186,12 +188,13 @@ class Article(object):
         if add_no_ne_label:
             counts = Counter([token.label for token in self.tokens])
         else:
-            counts = Counter([token.label for token in self.tokens if token.label != NO_NE_LABEL])
+            counts = Counter([token.label for token in self.tokens \
+                                          if token.label != cfg.NO_NE_LABEL])
         return counts.most_common()
-    
+
     def count_labels(self, add_no_ne_label=False):
         """Returns how many named entity tokens appear in the article/document.
-        
+
         Args:
             add_no_ne_label: Whether to also count unlabeled words. (Default is False.)
         Returns:
@@ -203,11 +206,12 @@ class Window(Article):
     """Encapsulates a small window of text/tokens."""
     def __init__(self, tokens):
         """Initialize a new Window object.
-        
+
         Args:
             tokens: The tokens/words contained in the text window, provided as list of Token
                 objects.
         """
+        super(Window, self).__init__("") # because pylint complains otherwise
         self.tokens = tokens
 
     def apply_features(self, features):
@@ -216,7 +220,7 @@ class Window(Article):
         token. Each of these lists can be empty. The lists are saved in the tokens and can later
         on be requested multiple times without the generation overhead (which can be heavy for
         some features).
-        
+
         Args:
             features: A list of feature generators from features.py .
         """
@@ -226,22 +230,22 @@ class Window(Article):
         # 3rd dimension: values (for this token and feature, usually just one value, sometimes more,
         #                        e.g. "w2vc=975")
         features_values = [feature.convert_window(self) for feature in features]
-        
+
         for token in self.tokens:
             token.feature_values = []
-        
+
         # After this, each self.token.feature_values will be a simple list
         # of feature values, e.g. ["w2v=875", "bc=48", ...]
         for feature_values in features_values:
-            assert type(feature_values) == type(list())
+            assert isinstance(feature_values, list)
             assert len(feature_values) == len(self.tokens)
-            
+
             for token_idx in range(len(self.tokens)):
                 self.tokens[token_idx].feature_values.extend(feature_values[token_idx])
-    
+
     def get_feature_values_list(self, word_index, skipchain_left, skipchain_right):
         """Generates a list of feature values (strings) for one token/word in the window.
-        
+
         Args:
             word_index: The index of the word/token for which to generate the featueres.
             skipchain_left: How many words to the left will be included among the features of
@@ -260,12 +264,16 @@ class Window(Article):
         end = min(len(self.tokens), word_index + 1 + skipchain_right)
         for i, token in enumerate(self.tokens[start:end]):
             diff = start + i - word_index
-            feature_values = ["%d:%s" % (diff, feature_value) for feature_value in token.feature_values]
+            feature_values = ["%d:%s" % (diff, feature_value) \
+                              for feature_value in token.feature_values]
             all_feature_values.extend(feature_values)
 
         return all_feature_values
 
     def get_labels(self):
+        """Returns the labels of all tokens as a list.
+        Returns:
+            list of strings"""
         return [token.label for token in self.tokens]
 
 class Token(object):
@@ -285,19 +293,19 @@ class Token(object):
         """
         self.original = original
         self.word = original
-        self.label = NO_NE_LABEL
+        self.label = cfg.NO_NE_LABEL
         if "/" in original:
             pos = original.rfind("/")
             end = original[pos+1:]
             # remove parts of BIO encoding, e.g. remove "B-" from "B-PER" or "I-" from "I-PER"
-            if REMOVE_BIO_ENCODING:
+            if cfg.REMOVE_BIO_ENCODING:
                 end = end.replace("B-", "").replace("I-", "")
-            if end in LABELS:
+            if end in cfg.LABELS:
                 self.word = original[0:pos]
                 self.label = end
         #self._word_ascii = None
         self.feature_values = None
-    
+
     # this was removed to get rid of the unicecode dependency and because the ascii representation
     # of words weren't used anyways
     #@property
