c874af9a9cbc969fd7393ac38bf833ea3058a5d6
aleju <kontakt@ajung.name>
2015-09-06 12:57:48 +0200

    Move constants to config, add german eval corpus handling

--- test.py
+++ test.py
@@ -8,49 +8,40 @@ from itertools import chain
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import LabelBinarizer
 
+"""
 from model.brown import BrownClusters
 from model.gazetteer import Gazetteer
 from model.lda import LdaWrapper
 from model.pos import PosTagger
 from model.unigrams import Unigrams
 from model.w2v import W2VClusters
-from model.datasets import load_windows, load_articles
-import model.features as features
+"""
+from model.datasets import load_windows, load_articles, Article
+#import model.features as features
 
 from train import create_features
+from config import *
 
 random.seed(42)
-
-CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-ARTICLES_FILEPATH = "/media/aj/grab/nlp/corpus/processed/wikipedia-ner/annotated-fulltext.txt"
-BROWN_CLUSTERS_FILEPATH = "/media/aj/ssd2a/nlp/corpus/brown/wikipedia-de/brown_c1000_min12/paths"
-#UNIGRAMS_FILEPATH = "/media/aj/ssd2a/nlp/corpus/processed/wikipedia-de/ngrams-1.txt"
-UNIGRAMS_FILEPATH = os.path.join(CURRENT_DIR, "preprocessing/unigrams.txt")
-UNIGRAMS_PERSON_FILEPATH = os.path.join(CURRENT_DIR, "preprocessing/unigrams_per.txt")
-LDA_FILEPATH = os.path.join(CURRENT_DIR, "preprocessing/lda_model")
-LDA_DICTIONARY_FILEPATH = os.path.join(CURRENT_DIR, "preprocessing/lda_dictionary")
-LDA_CACHE_MAX_SIZE = 100000
-STANFORD_DIR = "/media/aj/ssd2a/nlp/nlpjava/stanford-postagger-full-2013-06-20/stanford-postagger-full-2013-06-20/"
-STANFORD_POS_JAR_FILEPATH = os.path.join(STANFORD_DIR, "stanford-postagger-3.2.0.jar")
-STANFORD_MODEL_FILEPATH = os.path.join(STANFORD_DIR, "models/german-fast.tagger")
-POS_TAGGER_CACHE_FILEPATH = os.path.join(CURRENT_DIR, "pos.cache")
-UNIGRAMS_SKIP_FIRST_N = 0
-UNIGRAMS_MAX_COUNT_WORDS = 1000
-W2V_CLUSTERS_FILEPATH = "/media/aj/ssd2a/nlp/corpus/word2vec/wikipedia-de/classes1000_cbow0_size300_neg0_win10_sample1em3_min50.txt"
-WINDOW_SIZE = 50
-SKIPCHAIN_LEFT = 5
-SKIPCHAIN_RIGHT = 5
-LDA_WINDOW_LEFT_SIZE = 5
-LDA_WINDOW_RIGHT_SIZE = 5
-MAX_ITERATIONS = None
-COUNT_EXAMPLES = 1000
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--identifier", required=True,
                         help="A short name/identifier for your experiment, e.g. 'ex42b'.")
+    parser.add_argument("--mycorpus", required=False, action="store_const", const=True,
+                        help="Whether to test on your corpus, defined via the constant ARTICLES_FILEPATH.")
+    parser.add_argument("--gereval", required=False, action="store_const", const=True,
+                        help="Whether to test on the german eval corpus.")
     args = parser.parse_args()
     
+    if args.mycorpus:
+        test_on_mycorpus(args)
+    if args.gereval:
+        test_on_gereval(args)
+    if not args.mycorpus and not args.gereval:
+        print("Expected either --mycorpus or --gereval flag")
+
+def test_on_mycorpus(args):
     print("Loading tagger...")
     tagger = pycrfsuite.Tagger()
     tagger.open(args.identifier)
@@ -65,6 +56,8 @@ def main():
     all_feature_values_lists = []
     added = 0
     for window in windows:
+        # in train.py we skipped the first COUNT_WINDOWS_TEST windows (i.e. excluded them from
+        # training), so here we can just use them
         labels = window.get_labels()
         feature_values_lists = []
         for word_idx in range(len(window.tokens)):
@@ -74,13 +67,74 @@ def main():
         
         added += 1
         if added % 500 == 0:
-            print("Added %d of max %d windows" % (added, COUNT_EXAMPLES))
-        if added == COUNT_EXAMPLES:
+            print("Added %d of max %d windows" % (added, COUNT_WINDOWS_TEST))
+        if added == COUNT_WINDOWS_TEST:
             break
     
     print("Testing on %d windows..." % (len(all_feature_values_lists)))
     predicted_label_chains = [tagger.tag(fvlists) for fvlists in all_feature_values_lists]
     print(bio_classification_report(correct_label_chains, predicted_label_chains))
+
+def test_on_gereval(args):
+    print("Loading tagger...")
+    tagger = pycrfsuite.Tagger()
+    tagger.open(args.identifier)
+
+    print("Creating features...")
+    features = create_features()
+    
+    print("Loading windows...")
+    windows = load_windows(load_gereval(GEREVAL_FILEPATH), WINDOW_SIZE, features, only_labeled_windows=True)
+
+    correct_label_chains = []
+    all_feature_values_lists = []
+    added = 0
+    for window in windows:
+        labels = window.get_labels()
+        feature_values_lists = []
+        for word_idx in range(len(window.tokens)):
+            feature_values_lists.append(window.get_feature_values_list(word_idx, SKIPCHAIN_LEFT, SKIPCHAIN_RIGHT))
+        all_feature_values_lists.append(feature_values_lists)
+        correct_label_chains.append(labels)
+        
+        added += 1
+        if added % 500 == 0:
+            print("Added %d windows" % (added))
+    
+    print("Testing on %d windows..." % (len(all_feature_values_lists)))
+    predicted_label_chains = [tagger.tag(fvlists) for fvlists in all_feature_values_lists]
+    print(bio_classification_report(correct_label_chains, predicted_label_chains))
+
+def load_gereval(filepath):
+    lines = open(filepath, "r").readlines()
+    lines = [line.decode("utf-8").strip() for line in lines]
+    # remove lines that are comments
+    lines = filter(lambda line: line[0:1] != "#", lines)
+    # remove all empty lines
+    lines = filter(lambda line: len(line) > 0, lines)
+
+    sentence  = []
+    sentences = []
+
+    for line in lines:
+        blocks = line.split("\t")
+        (number, word, tag1, tag2) = blocks
+        number = int(number)
+        
+        if number == 1 and len(sentence) > 0:
+            #sentences.append(sentence)
+            sentences.extend(sentence)
+            sentence = []
+        
+        if "OTH" in tag1:
+            tag1 = "MISC"
+        
+        if any([label in tag1 for label in LABELS]):
+            sentence.append(word + "/" + tag1)
+        else:
+            sentence.append(word)
+
+    return [Article(" ".join(sentences))]
 
 def bio_classification_report(y_true, y_pred):
     """
