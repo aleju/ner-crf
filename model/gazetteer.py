94e7a16854e5ca2704af24b5cca071c9efd01614
aleju <kontakt@ajung.name>
2015-09-06 18:16:11 +0200

    Optimize styling according to pylint

--- model/gazetteer.py
+++ model/gazetteer.py
@@ -3,12 +3,12 @@
 A Gazetteer contains a set of words that are names (e.g. names of people)."""
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-class Gazetteer():
+class Gazetteer(object):
     """Class encapsulating a Gazetteer.
     A Gazetteer contains a set of words that are names (e.g. names of people)."""
     def __init__(self, unigrams_names, unigrams):
         """Initializes the gazetter and fills it from two unigrams list.
-        
+
         Args:
             unigrams_names: Unigrams object that should contain only names (e.g. only names of
                 people).
@@ -16,31 +16,28 @@ class Gazetteer():
         """
         self.gazetteer = set()
         self.fill_by_comparison(unigrams_names, unigrams)
-    
+
     def clear(self):
         """Resets/empties the Gazetteer."""
         self.gazetteer = set()
-    
+
     def fill_by_comparison(self, unigrams_names, unigrams):
         """Fills the Gazetteer automatically from two lists of unigrams (as described in Args).
         After filling the Gazetteer only contains those names which appeared more often
         among the names than among the whole corpus.
-        
+
         Args:
             unigrams_names: Unigrams object that should contain only names (e.g. only names of
                 people).
             unigrams: Unigrams object that should contain all words of the corpus.
         """
-        sum_names_counts = unigrams_names.sum_of_counts
-        sum_ug_counts = unigrams.sum_of_counts
-
         for name, _ in unigrams_names.word_to_count.iteritems():
             freq_names = unigrams_names.get_frequency_of(name)
             freq_all = unigrams.get_frequency_of(name)
-            
+
             if freq_all is None or freq_names > freq_all:
                 self.gazetteer.add(name)
-    
+
     def contains(self, word):
         """Returns whether the Gazetteer contains the provided word.
         Args:
@@ -48,4 +45,4 @@ class Gazetteer():
         Returns:
             True if the word is contained in the Gazetteer, False otherwise.
         """
-        return (word in self.gazetteer)
+        return word in self.gazetteer
