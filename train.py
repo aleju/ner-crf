feab47c4bc68481b0371d6f105b18afcbf115a2b
aleju <kontakt@ajung.name>
2015-09-06 17:39:57 +0200

    Optimize styling according to pylint

--- train.py
+++ train.py
@@ -8,7 +8,6 @@ Usage example:
     python train.py --identifier="my_experiment"
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
-import os
 import argparse
 import random
 import pycrfsuite
@@ -17,7 +16,7 @@ from model.datasets import load_windows, load_articles, generate_examples
 import model.features as features
 
 # All capitalized constants come from this file
-from config import *
+import config as cfg
 
 random.seed(42)
 
@@ -27,12 +26,12 @@ def main():
     parser.add_argument("--identifier", required=True,
                         help="A short name/identifier for your experiment, e.g. 'ex42b'.")
     args = parser.parse_args()
-    
+
     train(args)
 
 def train(args):
     """Main training method.
-    
+
     Does the following:
         1. Create a new pycrfsuite trainer object. We will have to add feature chains and label
            chains to that object and then train on them.
@@ -52,41 +51,42 @@ def train(args):
            for two tokens.
         5. Add feature chains and label chains to the trainer.
         6. Train. This may take several hours for 20k windows.
-    
+
     Args:
         args: Command line arguments as parsed by argparse.ArgumentParser.
     """
     trainer = pycrfsuite.Trainer(verbose=True)
-    
+
     # Create/Initialize the feature generators
     # this may take a few minutes
     print("Creating features...")
     feature_generators = features.create_features()
-    
+
     # Initialize the window generator
     # each window has a fixed maximum size of tokens
     print("Loading windows...")
-    windows = load_windows(load_articles(ARTICLES_FILEPATH), WINDOW_SIZE, feature_generators, only_labeled_windows=True)
-    
+    windows = load_windows(load_articles(cfg.ARTICLES_FILEPATH), cfg.WINDOW_SIZE,
+                           feature_generators, only_labeled_windows=True)
+
     # Add chains of features (each list of lists of strings)
     # and chains of labels (each list of strings)
     # to the trainer.
     # This may take a long while, especially because of the lengthy POS tagging.
     # POS tags and LDA results are cached, so the second run through this part will be significantly
     # faster.
-    print("Adding example windows (up to max %d)..." % (COUNT_WINDOWS_TRAIN))
-    examples = generate_examples(windows, nb_append=COUNT_WINDOWS_TRAIN, nb_skip=COUNT_WINDOWS_TEST,
-                                 verbose=True)
+    print("Adding example windows (up to max %d)..." % (cfg.COUNT_WINDOWS_TRAIN))
+    examples = generate_examples(windows, nb_append=cfg.COUNT_WINDOWS_TRAIN,
+                                 nb_skip=cfg.COUNT_WINDOWS_TEST, verbose=True)
     for feature_values_lists, labels in examples:
         trainer.append(feature_values_lists, labels)
-    
+
     # Train the model
     # this may take several hours
     print("Training...")
-    if MAX_ITERATIONS is not None and MAX_ITERATIONS > 0:
+    if cfg.MAX_ITERATIONS is not None and cfg.MAX_ITERATIONS > 0:
         # set the maximum number of iterations of defined in the config file
         # the optimizer stops automatically after some iterations if this is not set
-        trainer.set_params({'max_iterations': MAX_ITERATIONS})
+        trainer.set_params({'max_iterations': cfg.MAX_ITERATIONS})
     trainer.train(args.identifier)
 
 # ----------------
