889a2608d1d4038a8c7ee1c445530fd1750c00e0
aleju <kontakt@ajung.name>
2015-09-06 17:48:04 +0200

    Optimize styling according to pylint

--- preprocessing/collect_unigrams.py
+++ preprocessing/collect_unigrams.py
@@ -8,33 +8,32 @@
         Washington/LOC D.C./LOC is the capital of the U.S.
         The foobird is a special species of birds. It's commonly found on mars.
         ...
-    
+
     Execute via:
         python -m preprocessing/collect_unigrams
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
-import os
 from model.unigrams import Unigrams
 
 # All capitalized constants come from this file
-from config import *
+import config as cfg
 
 def main():
     """Main function. Gathers all unigrams and name-unigrams, see documantation at the top."""
-    
+
     # collect all unigrams (all labels, including "O")
     print("Collecting unigrams...")
     ug_all = Unigrams()
-    ug_all.fill_from_articles(ARTICLES_FILEPATH, verbose=True)
-    ug_all.write_to_file(UNIGRAMS_FILEPATH)
+    ug_all.fill_from_articles(cfg.ARTICLES_FILEPATH, verbose=True)
+    ug_all.write_to_file(cfg.UNIGRAMS_FILEPATH)
     ug_all = None
-    
+
     # collect only unigrams of label PER
     print("Collecting person names (label=PER)...")
     ug_names = Unigrams()
-    ug_names.fill_from_articles_labels(ARTICLES_FILEPATH, ["PER"], verbose=True)
-    ug_names.write_to_file(UNIGRAMS_PERSON_FILEPATH)
-    
+    ug_names.fill_from_articles_labels(cfg.ARTICLES_FILEPATH, ["PER"], verbose=True)
+    ug_names.write_to_file(cfg.UNIGRAMS_PERSON_FILEPATH)
+
     print("Finished.")
 
 # ---------------
